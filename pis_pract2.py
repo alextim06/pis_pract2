185b87e7d6294d58d47885863939f6d504715afe
Alexandr Timofeev <alex.tim06@mail.ru>
2026-02-11 13:19:30 +0700

    Добавил тип занятия в класс Lesson и создал классы наследники Lecture и Pracrice

--- pis_pract2.py
+++ pis_pract2.py
@@ -1,27 +1,29 @@
 class Lesson():
-    def __init__(self, date, auditorium, teacher, ):
+    def __init__(self, date, auditorium, teacher, lesson_type):
         self.date = date
         self.auditorium = auditorium
         self.teacher = teacher
+        self.lesson_type = lesson_type
 
     def __str__(self):
-        return f'Дата: {self.date}, Аудитория: {self.auditorium}, Имя преподавателя: {self.teacher}'
+        return f'Дата: {self.date}, Аудитория: {self.auditorium}, Имя преподавателя: {self.teacher}, Тип: {self.lesson_type}'
 
-lessons = []
-with open('date_prac1.txt', 'r', encoding='utf-8') as f:
-    for line in f.readlines():
-        parts = line.split('"')
+class Lecture(Lesson):
+    def __init__(self, date, auditorium, teacher, lesson_type, topic, students_count):
+        super().__init__(date, auditorium, teacher, lesson_type)
+        self.topic = topic
+        self.student_count = students_count
 
-        lesson = Lesson(
-        date=parts[0].strip(),
-        auditorium=parts[1],
-        teacher=parts[3],
-        )
-
-        lessons.append(lesson)
-
-for i in lessons:
-    print(i)
+    def __str__(self):
+        base_str = super().__str__()
+        return f'{base_str}, Тема: {self.topic}, Кол-во студентов: {self.student_count}'
 
 
+class Practice(Lesson):
+    def __init__(self, date, auditorium, teacher, lesson_type, task_count):
+        super().__init__(date, auditorium, teacher, lesson_type)
+        self.task_count = task_count
 
+    def __str__(self):
+        base_str = super().__str__()
+        return f'{base_str}, Кол-во задач: {self.task_count}'
